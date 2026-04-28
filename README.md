75175b71774a9bcf33e56cd27618ac162df5269f
Eh-o-Tuta <Matheuspsc.2018@gmail.com>
2026-04-28 00:09:31 -0300

    atualizações

---
+++ README.md
@@ -0,0 +1,67 @@
+# MyApp - E-commerce React Native
+
+Este é um aplicativo de e-commerce simples desenvolvido com React Native e React Navigation. Ele permite que os usuários se autentiquem, naveguem por categorias de produtos, visualizem a lista de produtos, e visualizem os detalhes de cada produto.
+
+## Funcionalidades
+
+- **Login**: Tela de Login: Permite que o usuário entre com um nome de usuário e senha. 
+- **Home**: Página de boas-vindas com botão para sair (logout) e acessar a lista de produtos.  
+- **Lista de Produtos**: Mostra todos os produtos cadastrados com nome, imagem e breve descrição.  
+- **Detalhes do Produto**: Ao selecionar um item da lista, o usuário vê informações completas como descrição, imagem e preço.  
+- **Logout**: Disponível na Home para encerrar a sessão e voltar ao login.
+
+## Tecnologias utilizadas
+
+- [React Native] Framework para desenvolvimento de aplicativos móveis.
+- [React Navigation] Biblioteca para navegação entre telas.
+- Axios - Cliente HTTP para fazer requisições à API.
+- Expo - Ferramenta que facilita o desenvolvimento e execução de aplicativos React Native.
+- JavaScript ES6+ 
+
+## Estrutura do projeto
+
+```
+src/
+ ├── components/
+ │   ├── LogoutButton.js        // Botão para sair da conta
+ │   └── ProductItem.js         // Exibição de item individual da lista
+ ├── navigation/
+ │   └── AppNavigation.js       // Rotas e navegação do app
+ ├── screens/
+ │   ├── HomeScreen.js          // Tela inicial
+ │   ├── LoginScreen.js         // Tela de login
+ │   ├── ProductDetailScreen.js // Tela de detalhes
+ │   └── ProductListScreen.js   // Lista de produtos
+```
+
+## Pré-requisitos
+
+- [Node.js]  
+- [Expo CLI]
+- Git instalado  
+
+## Login e senha para acessar o app
+ - Login: Admin
+ - Senha: 1234
+
+## Como rodar o projeto
+
+```bash
+# Clonar o repositório
+git clone https://github.com/seu-usuario/my-app.git
+
+# Acessar a pasta do projeto
+cd my-app
+
+# Instalar a dependência
+npm install
+
+Para iniciar o app:
+
+```bash
+
+npx expo start
+
+
+Autor
+- Maheus Souza
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,81 @@
+import React, { useLayoutEffect } from 'react';
+import { View, Text, Button, ScrollView, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const categoriasMasculinas = [
+    { nome: 'Camisas Masculinas', valor: 'mens-shirts' },
+    { nome: 'Sapatos Masculinos', valor: 'mens-shoes' },
+    { nome: 'Relógios Masculinos', valor: 'mens-watches' },
+  ];
+
+  const categoriasFemininas = [
+    { nome: 'Bolsas Femininas', valor: 'womens-bags' },
+    { nome: 'Vestidos Femininos', valor: 'womens-dresses' },
+    { nome: 'Joias Femininas', valor: 'womens-jewellery' },
+    { nome: 'Sapatos Femininos', valor: 'womens-shoes' },
+    { nome: 'Relógios Femininos', valor: 'womens-watches' },
+  ];
+
+  const irParaCategoria = (categoria) => {
+    navigation.navigate('ProductList', { categoria });
+  };
+
+  const sair = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Login' }],
+      
+    });
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => 
+    <Button title="Sair"
+      onPress={sair} 
+      color='#5e0d75'
+      />,
+    });
+  }, [navigation]);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.titulo}>Categorias Masculinas</Text>
+      {categoriasMasculinas.map((cat) => (
+        <Button
+          key={cat.valor}
+          title={cat.nome}
+          onPress={() => irParaCategoria(cat.valor)}
+          color='#5e0d75'
+        />
+      ))}
+
+      <Text style={styles.titulo}>Categorias Femininas</Text>
+      {categoriasFemininas.map((cat) => (
+        <Button
+          key={cat.valor}
+          title={cat.nome}
+          onPress={() => irParaCategoria(cat.valor)}
+          color='#5e0d75'
+        />
+      ))}
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  titulo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 16,
+    marginBottom: 8,
+  },
+});
